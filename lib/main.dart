221f38591f93c10b16e565186c91bfe8a5a94f2d
minntoko <minntoko0822@gmail.com>
2023-09-09 12:34:53 +0900

    add: colum, row編

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const a = MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text("りんご")
-      )
-    )
+  // ignore: prefer_const_constructors
+  final col = Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Text("りんごパイ"),
+        Text("ぶどう"),
+        Text("レモン"),
+      ]);
+
+  final row = Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [col, col, col],
   );
+  ;
+
+  final a = MaterialApp(home: Scaffold(body: Center(child: row)));
 
   runApp(a);
 }
---
+++ lib/widget/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  const a = MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text("りんご")
+      )
+    )
+  );
+
+  runApp(a);
+}

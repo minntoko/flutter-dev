27fab4baad730522cea91d57eec7660efab68dca
minntoko <minntoko0822@gmail.com>
2023-09-18 18:26:55 +0900

    add: Carousel編

---
+++ lib/freezed/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myapp/freezed/fish.dart';
+
+void main() {
+  const app = MaterialApp(home: Home());
+  const scope = ProviderScope(child: app);
+  runApp(scope);
+}
+
+final fishProvider =
+    StateProvider((ref) => const Fish(name: 'マグロ', size: 200, price: 300));
+
+class Home extends ConsumerWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final fish = ref.watch(fishProvider);
+    final nameText = Text(
+      '名前: ${fish.name}',
+      style: const TextStyle(fontSize: 20),
+    );
+    final sizeText = Text(
+      '大きさ: ${fish.size}cm',
+      style: const TextStyle(fontSize: 20),
+    );
+    final priceText = Text(
+      '値段: ${fish.price}万円',
+      style: const TextStyle(fontSize: 20),
+    );
+
+    final button = ElevatedButton(
+        onPressed: () => onPressed(ref), child: const Text('変更する'));
+
+    final col = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [nameText, sizeText, priceText, button],
+    );
+    return Scaffold(
+        body: Center(
+      child: col,
+    ));
+  }
+
+  void onPressed(WidgetRef ref) {
+    final fish = ref.read(fishProvider);
+    final newFish = fish.copyWith(
+      price: 500,
+    );
+    ref.read(fishProvider.notifier).state = newFish;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:myapp/fish.dart';
+import 'package:myapp/listView/main.dart';
 
 void main() {
-  const app = MaterialApp(home: Home());
-  const scope = ProviderScope(child: app);
-  runApp(scope);
+  const app = MaterialApp(
+    home: Home(),
+  );
+  runApp(app);
 }
 
-final fishProvider =
-    StateProvider((ref) => const Fish(name: 'マグロ', size: 200, price: 300));
+// 馬のモデルクラス
+class Horse {
+  final String name;
+  final String iconUrl;
+  Horse(this.name, this.iconUrl);
+}
 
-class Home extends ConsumerWidget {
+// カードにしたいモデルたち
+final models = [
+  Horse('ブラックダイヤモンド', 'horse1.png'),
+  Horse('スターライト', 'horse2.png'),
+  Horse('ゴールデンウィンド', 'horse3.png'),
+  Horse('ファイアストライク', 'horse4.png'),
+  Horse('サンダーランチョ', 'horse5.png'),
+];
+
+// 馬のカード Widget
+class HorseCard extends StatelessWidget {
+  const HorseCard({super.key, required this.model});
+
+  final Horse model;
+
+  @override
+  Widget build(BuildContext context) {
+    // 画像
+    final img = SizedBox(
+      height: 100,
+      child: Image.asset('assets/images/${model.iconUrl}'),
+    );
+
+    // 名前
+    final text = Text(
+      model.name,
+      style: const TextStyle(fontSize: 20),
+    );
+
+    final imgAndText = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [img, text],
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white70,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 5,
+            blurRadius: 10,
+            offset: const Offset(0, 0),
+          ),
+        ],
+      ),
+      child: imgAndText,
+    );
+  }
+}
+
+// モデル => ウィジェットに変換する関数
+Widget modelToWidget(Horse model) {
+  return Container(
+    padding: const EdgeInsets.all(10),
+    child: HorseCard(
+      model: model,
+    ),
+  );
+}
+
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final fish = ref.watch(fishProvider);
-    final nameText = Text(
-      '名前: ${fish.name}',
-      style: const TextStyle(fontSize: 20),
-    );
-    final sizeText = Text(
-      '大きさ: ${fish.size}cm',
-      style: const TextStyle(fontSize: 20),
-    );
-    final priceText = Text(
-      '値段: ${fish.price}万円',
-      style: const TextStyle(fontSize: 20),
-    );
+  Widget build(BuildContext context) {
+    // カルーセル
+    final carousel = Container(
+        height: 200,
+        color: Colors.white,
+        child: PageView.builder(
+          controller: PageController(viewportFraction: 0.6),
+          itemCount: models.length,
+          itemBuilder: (c, i) => modelToWidget(models[i]),
+        ));
 
-    final button = ElevatedButton(
-        onPressed: () => onPressed(ref), child: const Text('変更する'));
-
-    final col = Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [nameText, sizeText, priceText, button],
-    );
     return Scaffold(
-        body: Center(
-      child: col,
-    ));
-  }
-
-  void onPressed(WidgetRef ref) {
-    final fish = ref.read(fishProvider);
-    final newFish = fish.copyWith(
-      price: 500,
+      appBar: AppBar(title: const Text('カルーセル編')),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: const Text(
+              '馬のカード一覧',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          carousel,
+        ],
+      )),
     );
-    ref.read(fishProvider.notifier).state = newFish;
   }
 }
